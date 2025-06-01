d7da4b6c889349980ad70a14cc115a0fedacee2d
semikoh <ejffjeosms@gmail.com>
2025-06-01 10:30:59 +0900

    프로젝트 추가

---
+++ apps/ui-docs/.storybook/preview.js
@@ -0,0 +1,17 @@
+import _ from '../node_modules/@bami-design-ui/ui/dist/styles/design-system.scss'
+
+console.log('_', _) // stoy book은 tree shaking에서 걸러짐, tree shaking에 걸리지 않도록 log 추가
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+    parameters: {
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
+    },
+}
+
+export default preview
