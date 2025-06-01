6b604bfb9f853b1c16d772f1d6bba752e6dce328
semikoh <ejffjeosms@gmail.com>
2025-06-01 12:34:37 +0900

    lint 수정

--- packages/ui/src/ListRow/index.tsx
+++ packages/ui/src/ListRow/index.tsx
@@ -1,4 +1,4 @@
-import {HTMLAttributes, ReactNode} from 'react'
+import type {HTMLAttributes, ReactNode} from 'react'
 import classNames from 'classnames/bind'
 import {Flex} from '../Flex'
 import {Text} from '../Text'
